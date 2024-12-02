b4aa5881b6da632a5e6d14b4c0974ad43099bab1
boiwantlearncode <ibetank@gmail.com>
2024-12-02 10:08:44 +0800

    Final version

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Task Tracker CLI
 
 Project #1 from a series of projects in roadmap.sh
+
+### How to use
+```bash
+# Adding a new task
+node task-cli add "Buy groceries"
+# Output: Task added successfully (ID: ...)
+
+# Updating and deleting tasks
+node task-cli update <ID> "Buy groceries and cook dinner"
+node task-cli delete <ID>
+
+# Marking a task as in progress or done
+node task-cli mark-in-progress <ID>
+node task-cli mark-done <ID>
+
+# Listing all tasks
+node task-cli list
+
+# Listing tasks by status
+node task-cli list done
+node task-cli list todo
+node task-cli list in-progress
+```
--- task-cli.js
+++ task-cli.js
@@ -6,29 +6,19 @@ const path = require('path');
 const program = new Command();
 const databaseFilePath = path.join(__dirname, 'database.json');
 
-// Function to read the JSON file
 function readDatabase() {
   if (!fs.existsSync(databaseFilePath)) {
-      // If the file doesn't exist, create an empty JSON object
       fs.writeFileSync(databaseFilePath, JSON.stringify({}), 'utf8');
   }
   const data = fs.readFileSync(databaseFilePath, 'utf8');
   return JSON.parse(data);
 }
 
-// Function to write to the JSON file
 function writeDatabase(data) {
-  fs.writeFileSync(databaseFilePath, JSON.stringify(data, null, 2), 'utf8'); // Pretty-print with 2 spaces
+  fs.writeFileSync(databaseFilePath, JSON.stringify(data, null, 2), 'utf8');
 }
 
-// const DUMMY_TASK = {
-//   id: 1,
-//   description: "Buy groceries",
-//   status: "todo | in-progress | done",
-//   createdAt: "12012012",
-//   updatedAt: "12012012"
-// }
-
+const validStatuses = ["todo", "in-progress", "done"]
 
 program
   .name('task-cli')
@@ -39,31 +29,33 @@ program
   .command('add')
   .description('Add a single task')
   .argument('<description>', 'Description of the task')
-  .action((description) => {
+  .action((desc) => {
     const currentDatetime = new Date().toLocaleString();
     const id = uuidv4();
+    const database = readDatabase();
 
     const task = {
-      id: id,
-      description: str,
+      description: desc,
       status: "todo",
       createdAt: currentDatetime,
       updatedAt: currentDatetime
     }
 
-    database = {...database, task};
+    database[id] = task;
+    writeDatabase(database);
+    console.log(database);
     console.log(`Task added successfully (ID: ${id})`);
   });
 
 program
   .command('update')
-  .description('Add a single task')
+  .description('Update a task based on id')
   .argument('<id>', 'ID of the task to update')
   .argument('<description>', 'New description of the task')
   .action((id, description) => {
     const currentDatetime = new Date().toLocaleString();
+    const database = readDatabase();
 
-    // Get first argument
     const prevTask = database[id];
     if (!prevTask) {
       console.log(`Task with ID ${id} not found.`);
@@ -72,32 +64,145 @@ program
 
     // Change description property of database to second argument
     const task = {
-      id: prevTask.id,
       description: description,
       status: prevTask.status,
       createdAt: prevTask.createdAt,
       updatedAt: currentDatetime
     }
 
-    database = {...database, task};
+    database[id] = task;
+    writeDatabase(database);
     console.log(database);
-    console.log(`Task added successfully (ID: ${id}`);
+    console.log(`Task updated successfully (ID: ${id})`);
   });
 
 program
-  .command('join')
-  .description('Join the command-arguments into a single string')
-  .argument('<strings...>', 'one or more strings')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((strings, options) => {
-    console.log(strings.join(options.separator));
+  .command('delete')
+  .description('Delete a task based on id')
+  .argument('<id>', 'ID of the task to delete')
+  .action((id) => {
+    const database = readDatabase();
+
+    const prevTask = database[id];
+    if (!prevTask) {
+      console.log(`Task with ID ${id} not found.`);
+      return;
+    }
+
+    delete database[id];
+
+    writeDatabase(database);
+    console.log(database);
+    console.log(`Task deleted successfully (ID: ${id})`);
+  });
+
+program
+  .command('mark-in-progress')
+  .description('Set the status of a task to \'in-progress\'')
+  .argument('<id>', 'ID of the task')
+  .action((id) => {
+    const currentDatetime = new Date().toLocaleString();
+    const database = readDatabase();
+
+    const prevTask = database[id];
+    if (!prevTask) {
+      console.log(`Task with ID ${id} not found.`);
+      return;
+    }
+
+    const task = {
+      description: prevTask.description,
+      status: 'in-progress',
+      createdAt: prevTask.createdAt,
+      updatedAt: currentDatetime
+    }
+
+    database[id] = task;
+    writeDatabase(database);
+    console.log(database);
+    console.log(`Status of task with ID ${id} has been updated to "in-progress".`);
+  });
+
+program
+  .command('mark-done')
+  .description('Set the status of a task to \'done\'')
+  .argument('<id>', 'ID of the task')
+  .action((id) => {
+    const currentDatetime = new Date().toLocaleString();
+    const database = readDatabase();
+
+    const prevTask = database[id];
+    if (!prevTask) {
+      console.log(`Task with ID ${id} not found.`);
+      return;
+    }
+
+    const task = {
+      description: prevTask.description,
+      status: 'done',
+      createdAt: prevTask.createdAt,
+      updatedAt: currentDatetime
+    }
+
+    database[id] = task;
+    writeDatabase(database);
+    console.log(database);
+    console.log(`Status of task with ID ${id} has been updated to "in-progress".`);
+  });
+
+program
+  .command('list')
+  .description('View all tasks')
+  .argument('[status]', 'todo | in-progress | done') // Optiona argument
+  .action((status) => {
+    if (status && !validStatuses.includes(status)) {
+      console.log(`Invalid status, please key in one of the following: todo | in-progress | done`);
+      return;
+    }
+
+    const database = readDatabase();
+    let tasks;
+
+    if (status) {
+      tasks = Object.values(database)
+                    .filter((task) => task.status == status)
+                    .map((task) => ({
+                      "Description": task.description,
+                      "Status": task.status,
+                      "Created At": task.createdAt.split(",")[0], // Extract only the date part
+                    }));
+    } else {
+      tasks = Object.values(database)
+                    .map((task) => ({
+                      "Description": task.description,
+                      "Status": task.status,
+                      "Created At": task.createdAt.split(",")[0], // Extract only the date part
+                    }));
+    }
+    console.table(tasks);
+
   });
 
 program.parse();
 
-// Try the following:
-//    node task-cli add "Buy groceries"
-//    node task-cli update <ID> "Buy even more groceries"
-//    node string-util split --separator=/ a/b/c
-//    node string-util split --first a,b,c
-//    node string-util join a b c d
+/*
+# Adding a new task
+node task-cli add "Buy groceries"
+# Output: Task added successfully (ID: ...)
+
+# Updating and deleting tasks
+node task-cli update <ID> "Buy groceries and cook dinner"
+node task-cli delete <ID>
+
+# Marking a task as in progress or done
+node task-cli mark-in-progress <ID>
+node task-cli mark-done <ID>
+
+# Listing all tasks
+node task-cli list
+
+# Listing tasks by status
+node task-cli list done
+node task-cli list todo
+node task-cli list in-progress
+*/
